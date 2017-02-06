bb714987ceb199a979403649079525bfda23fdd3
Devon Bray <dev@esologic.com>
2017-02-06 12:41:12 -0500

    starting to work on IMU

---
+++ pocs/imu/imu.ino
@@ -0,0 +1,11 @@
+#include <SparkFunLSM9DS1.h> // SparkFun LSM9DS1 library
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
